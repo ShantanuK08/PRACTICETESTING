563451256441dcba8376c1d4ed497ae1af5ee5bd
shantanu <shantanukavathekar5@gmail.com>
2025-01-01 17:21:58 +0530

    update

--- tests.py
+++ tests.py
@@ -1,19 +1,20 @@
 import requests
 import pytest
+import json
 
 class TestAPIS:
     def test_01_listusers(self):
         print("Entry")
         assert True  # Include an assertion to mark the test as valid
         
-    Header = {
-  "Page Details": {
-    "page": 2,
-    "per_page": 6,
-    "total": 12,
-    "total_pages": 2
-  }
-}
+        header = {}
+        body = {}
 
-        
-    print("Exit")
+        url="https://reqres.in/api/users?page=2"
+        response = requests.get(url)
+        response_json = response.json()
+        print()
+        assert response.status_code == 200
+        assert response_json["data"] is not None, "Response does not contain 'data' key"
+        assert response.json()['page'] == 2
+        print("Exit")
