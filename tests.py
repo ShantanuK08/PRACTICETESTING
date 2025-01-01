6947c6e16a294715963960cdd23f9882368d46b5
shantanu <shantanukavathekar5@gmail.com>
2025-01-01 16:34:15 +0530

    update

---
+++ tests.py
@@ -0,0 +1,19 @@
+import requests
+import pytest
+
+class TestAPIS:
+    def test_01_listusers(self):
+        print("Entry")
+        assert True  # Include an assertion to mark the test as valid
+        
+    Header = {
+  "Page Details": {
+    "page": 2,
+    "per_page": 6,
+    "total": 12,
+    "total_pages": 2
+  }
+}
+
+        
+    print("Exit")
