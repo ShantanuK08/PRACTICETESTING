c1f01eb26fc320644f3d153f8014c4909f46d1c4
shantanu <shantanukavathekar5@gmail.com>
2025-01-01 16:25:32 +0530

    update

---
+++ tests/tests.py
@@ -0,0 +1,10 @@
+import requests
+import pytest
+
+class TestAPIS:
+    def test_01_listusers(self):
+        print("Entry")
+        assert True  # Include an assertion to mark the test as valid
+
+        
+        print("Exit")
